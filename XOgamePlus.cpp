d33e996808ed4e22e8f72737ef59f64699ff1132
VasiliiSokolov <vasilysokolov@mail.ru>
2022-09-18 16:31:11 +0300

    Добавьте файлы проекта.

---
+++ XOgamePlus.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+int main()
+{
+    int iInput, jInput;
+    char move;
+    char battleField[3][3] =
+    {
+        {0,0,0},
+        {0,0,0},
+        {0,0,0}
+    };
+    for(int count = 0; count < 10; count++)
+    {
+        if (count % 2 == 0)
+        {
+            move = 'X';
+        }
+        else
+        {
+            move = 'O';
+        }
+        std::cout << "Player " << move << ", make a move: \n";
+        std::cin >> iInput >> jInput;
+        battleField[iInput][jInput] = move;
+    }
+    
+}
+
